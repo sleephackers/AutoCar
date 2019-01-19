817a15f4f69a0614257994335f4f6d72287d9d7c
Roshini <grannyismylife@gmail.com>
2019-01-19 22:51:00 +0530

    Fails to get location

--- app/src/main/java/com/example/root/myautocar/SimulationActivity.java
+++ app/src/main/java/com/example/root/myautocar/SimulationActivity.java
@@ -1,6 +1,15 @@
 package com.example.root.myautocar;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,7 +27,9 @@ public class SimulationActivity extends AppCompatActivity implements AdapterView
     public int fuel,gear;
     private Spinner spinner;
     private Boolean isbrakepressed=false,accpressed=false;
+    LocationManager locationManager;
     Button brake,accelerator,refill,collision;
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 16;
     TextView speedometer,fueltext;
     private static final String[] gears = {"1","2","3","4","5","6"};
     private Handler handler;
@@ -54,8 +65,20 @@ public class SimulationActivity extends AppCompatActivity implements AdapterView
         };
 
         handler.postDelayed(runnable, 5000);
-
-
+//        Log.e("poda", "onCreate: "+ checkLocationPermission());
+//        if (checkLocationPermission()==true) {
+//            Log.e("HI3", "returned checkLocationPermission true ");
+//
+//            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//
+//            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+//
+//            Log.e("HIII", "Location: "+location.getLatitude()+" "+ location.getLongitude());
+//        } else {
+//            Log.e("ERROR LOCATION", "Check location permission returned false");
+//            Log.e("HI3", "returned checkLocationPermission false ");
+//
+//        }
 
         brake.setOnTouchListener(new RepeatListener(400, 75, new View.OnClickListener() {
             @Override
@@ -74,9 +97,13 @@ public class SimulationActivity extends AppCompatActivity implements AdapterView
             @Override
             public void onClick(View v) {
                 Log.e("CHCEK", "In OnClick");
+                if (gear == 6)
+                    speed++;
+                else {
+                        speed +=gear;
+                        speedometer.setText("" + speed);
 
-                speed+=gear;
-                speedometer.setText("" + speed);
+                }
             }
         }));
 
@@ -104,12 +131,54 @@ public class SimulationActivity extends AppCompatActivity implements AdapterView
         spinner.setOnItemSelectedListener(this);
 
     }
+//
+//    public boolean checkLocationPermission() {
+//        Log.e("HI1", "Inside checkLocationPermission ");
+//        if (ContextCompat.checkSelfPermission(this,
+//                Manifest.permission.ACCESS_FINE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED) {
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+//                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+//
+//                new AlertDialog.Builder(this)
+//                        .setTitle("Permission")
+//                        .setMessage("App requires your location")
+//                        .setPositiveButton("Turn On", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialogInterface, int i) {
+//                                ActivityCompat.requestPermissions(SimulationActivity.this,
+//                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+//                                        MY_PERMISSIONS_REQUEST_LOCATION);
+//                                Log.e("HI2", "Location Accepted ");
+//
+//                            }
+//                        })
+//                        .setNegativeButton("Dont Allow", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialogInterface, int i) {
+//                                Toast.makeText(getApplicationContext(), "Various Functionalities won't work", Toast.LENGTH_SHORT).show();
+//                            }
+//                        })
+//                        .create()
+//                        .show();
+//
+//
+//            } else {
+//                ActivityCompat.requestPermissions(this,
+//                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+//                        MY_PERMISSIONS_REQUEST_LOCATION);
+//            }
+//            return false;
+//        } else {
+//            return true;
+//        }
+//    }
 
     //Use this function to send data every 10 seconds
 
     private void sendstats()
     {
-        if(speed==0);
+        if(speed==0 || fuel==0);
         else {
             fuel--;
             fueltext.setText("" + fuel);
